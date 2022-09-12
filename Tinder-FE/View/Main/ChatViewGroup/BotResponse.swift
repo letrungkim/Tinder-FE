9002e052c8f98ea0e49ea37cf9572aa48ec7e38f
Gia Huy <giahuy@Gias-MacBook-Pro.local>
2022-09-12 21:48:51 +0700

    complete chatBot

---
+++ Tinder-FE/View/Main/ChatViewGroup/BotResponse.swift
@@ -0,0 +1,8 @@
+//
+//  BotResponse.swift
+//  Tinder-FE
+//
+//  Created by Gia Huy on 12/09/2022.
+//
+
+import Foundation
---
+++ Tinder-FE/View/Main/ChatViewGroup/ChatBotViewScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ChatBotViewScreen.swift
+//  Tinder-FE
+//
+//  Created by Gia Huy on 12/09/2022.
+//
+
+import SwiftUI
+
+struct ChatBotViewScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatBotViewScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatBotViewScreen()
+    }
+}
